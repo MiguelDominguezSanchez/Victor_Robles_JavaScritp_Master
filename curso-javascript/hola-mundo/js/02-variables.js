98f3dcab65502917c719b870cf306ba17420abef
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-25 16:00:21 +0200

    18. Variables y comentarios

---
+++ curso-javascript/hola-mundo/js/02-variables.js
@@ -0,0 +1,14 @@
+// VARIABLES
+// una variable es un contenedor de información
+
+var pais = 'España'
+var continente = 'Europa'
+var antiguedad = 2019
+// concatenacion de dos cadenas de texto en una sola
+
+pais = 'Mexico'
+continente = 'Latinoamerica'
+var pais_y_continente = pais + '' + continente
+
+console.log(pais, continente, antiguedad)
+alert(pais_y_continente)
