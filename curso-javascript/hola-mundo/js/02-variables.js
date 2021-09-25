746df3b3db6a04ae39cbdc15681adf4f91b5d236
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-25 16:16:40 +0200

    20. Let y Var

--- curso-javascript/hola-mundo/js/02-variables.js
+++ curso-javascript/hola-mundo/js/02-variables.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // VARIABLES
 // una variable es un contenedor de información
 
@@ -5,10 +7,12 @@ var pais = 'España'
 var continente = 'Europa'
 var antiguedad = 2019
 // concatenacion de dos cadenas de texto en una sola
+var pais_y_continente = pais + '' + continente
+let prueba = 'hola'
+alert('hola')
 
 pais = 'Mexico'
 continente = 'Latinoamerica'
-var pais_y_continente = pais + '' + continente
 
 console.log(pais, continente, antiguedad)
 alert(pais_y_continente)
