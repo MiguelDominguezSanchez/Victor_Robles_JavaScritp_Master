34a2ce65dee5b739f7e64c05747d360ae1593404
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-25 18:17:17 +0200

    25. Condicionales y operadores lógicos

--- curso-javascript/hola-mundo/js/06-condicionales.js
+++ curso-javascript/hola-mundo/js/06-condicionales.js
@@ -44,3 +44,42 @@ if (edad >= 18) {
 	// Es menor de edad
 	console.log(nombre + ' tiene ' + edad + ' años, es MENOR de edad')
 }
+
+/*
+// Operadores lógico
+AND(Y): &&
+OR(O): ||
+Negación: !
+*/
+
+// var year = 2018
+
+// Negación
+if (year != 2016) {
+	console.log('El año no es 2016, realmente es: ' + year)
+}
+
+/*
+// AND
+if (year >= 2000 && year <= 2020 && year != 2018) {
+	console.log('Estamos en la era actual')
+} else {
+	console.log('Estamos en la era postmoderna')
+}
+*/
+
+var year = 2022
+
+// AND
+if (year >= 2000 && year <= 2020) {
+	console.log('Estamos en la era actual')
+} else {
+	console.log('Estamos en la era post moderna')
+}
+
+// OR
+if (year == 2008 || (year >= 2018 && year == 2028)) {
+	console.log('el año acaba en 8')
+} else {
+	console.log('Año no registrado')
+}
