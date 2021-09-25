8491a336f8c4223cc3fcfb0a77726f80e0fb317b
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-25 16:42:29 +0200

    23. Detectar el tipo de dato con Typeof

--- curso-javascript/hola-mundo/js/05-tipos-de-datos.js
+++ curso-javascript/hola-mundo/js/05-tipos-de-datos.js
@@ -11,7 +11,9 @@ var operacion = numero1 * numero2
 var numero_entero = 44
 var cadena_texto = 'Hola que tal'
 var verdadero_o_falso = false
-
 var numero_falso = '33.4'
 
-console.log(String(numero_entero) + 4)
+console.log(typeof numero_entero)
+console.log(typeof cadena_texto)
+console.log(typeof verdadero_o_falso)
+console.log(typeof numero_falso)
